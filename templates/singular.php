7411a952489843c94f4725fe3aaad7e48ffea089
Elio Rivero <eliorivero@gmail.com>
2015-10-06 10:36:54 -0300

    Remove unnecessary pingback URL in template.

--- templates/singular.php
+++ templates/singular.php
@@ -11,7 +11,6 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php wp_head(); ?>
 </head>
 
