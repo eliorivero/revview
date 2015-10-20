9364f184216f2c49d6719de530489c528106af78
Elio Rivero <eliorivero@gmail.com>
2015-10-20 16:03:33 -0300

    Prevent iOS zoom when select control is touched.

--- templates/singular.php
+++ templates/singular.php
@@ -9,7 +9,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	<meta name="robots" content="noindex, nofollow">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
